e51e0cdd783e96bfb39ee789f501e15cee43af8b
Elazar <96201125+awakzdev@users.noreply.github.com>
2023-03-11 13:30:20 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -70,29 +70,42 @@ resource "google_storage_bucket_object" "archive" {
 
 //////////////////////////////
 
-# Cloud Function resource 
 resource "google_cloudfunctions_function" "secret_test_func" {
   for_each = local.functions
-
-  name        = each.key
-  description = "Test - ${each.key}"
-  runtime     = "nodejs16"
+  
   region      = var.gcp_functions_region
+  name        = each.key
+  description = "${each.key}-Cloud-Function"
+  runtime     = "nodejs16"
   entry_point = "catalog_sync" # Entrypoint should be a function in the code
 
-  # vpc_connector = google_vpc_access_connector.serverless_vpc.id
+  vpc_connector = google_vpc_access_connector.serverless_vpc.id
   source_archive_bucket = google_storage_bucket.function_bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
-
   available_memory_mb   = each.value.available_memory_mb
   timeout               = each.value.timeout
-  trigger_http = lookup(each.value, "httpTrigger", null) == null ? null : true
+
+  // PRODUCTION ONLY
+  # runtime     = each.value.runtime
+  # entry_point = each.value.entrypoint
+
+  // Cloud Functions with secret must contains IAM permissions 'roles/secretmanager.secretAccessor' otherwise your function will fail.
+  # service_account_email = google_service_account.cloud_functions_service_account.email
+
+  trigger_http = lookup(each.value, "httpsTrigger", {}) == {} ? null : true
   
   dynamic "event_trigger" {
-    for_each   = lookup(each.value, "eventTrigger", {})
+    for_each = contains(keys(each.value), "eventTrigger") ? [1] : []
     content {
-      event_type = event_trigger.value.eventType
-      resource   = event_trigger.value.resource
+      event_type = each.value.eventTrigger.eventType
+      resource   = each.value.eventTrigger.resource
+
+      dynamic "failure_policy" {
+        for_each = contains(keys(each.value.eventTrigger.failurePolicy), "retry") ? [1] : []
+        content {
+          retry = true
+        }
+      }
     }
   }
 
@@ -103,7 +116,7 @@ resource "google_cloudfunctions_function" "secret_test_func" {
   } : null
 
   # Build Environment Variables
-  build_environment_variables = lookup(each.value, "build_environment_variables", null) != null ?{
+  build_environment_variables = lookup(each.value, "build_environment_variables", null) != null ? {
     for key, value in each.value.buildEnvironmentVariables :
     key => value
   } : null
@@ -141,5 +154,12 @@ resource "google_cloudfunctions_function" "secret_test_func" {
     ignore_changes = [
       source_archive_bucket,source_archive_object,labels["deployment-tool"]
     ]
+  }
+
+  # Setting timeouts for resources
+  # Default is 5 but terraform might time out before creation / update is complete.
+  timeouts {
+    create = "10m"
+    update = "10m"
   }
 }
