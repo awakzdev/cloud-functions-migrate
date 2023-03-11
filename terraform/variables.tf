f652d37ec13f61b8fcae21d58138fc1c0539276e
Elazar <96201125+awakzdev@users.noreply.github.com>
2023-03-11 22:18:12 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,10 +6,6 @@ variable "gcp_region" {
   description = "region"
 }
 
-variable "name_prefix" {
-  description = "naming prefix for resources"
-}
-
 variable "gcp_functions_region" {
   description = "Region to deploy the GCP functions. Some newer regions such as me-west1 don't support functions yet so it might be required to use a different region for the functions"
 }
